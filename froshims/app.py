fdf175c48aa8920f672059ef60b230d1547422b1
Ambar Santos <santosambar738@gmail.com>
2026-05-17 17:21:44 -0400

    Update app.py

--- froshims/app.py
+++ froshims/app.py
@@ -8,3 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
   return render_template("index.html")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+  return render_template("success.html")
