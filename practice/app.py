794d817d185f7e45d610867f88fccb4436e008d7
Ambar Santos <santosambar738@gmail.com>
2026-05-31 23:41:37 -0400

    Status code in view function

--- practice/app.py
+++ practice/app.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p> Your browser is {}</p'.format(user_agent)
+    return '<h1>Bad Request</h1>', 400
 
 @app.route('/user/<name>')
 def user(name):
