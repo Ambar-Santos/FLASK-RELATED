c01ad7e8ea2f46313ec0fffce59c138545b57366
Ambar Santos <santosambar738@gmail.com>
2026-06-01 13:36:20 -0400

    Adding some bootstrap and the bootstrap package?

--- practice/app.py
+++ practice/app.py
@@ -1,8 +1,10 @@
 #Initialization 
 from flask import Flask, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
