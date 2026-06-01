2a112b513e98c1cb4b9a1bae6f12bf8429a2a5e4
Ambar Santos <santosambar738@gmail.com>
2026-05-31 23:34:48 -0400

    request object

--- practice/app.py
+++ practice/app.py
@@ -1,11 +1,14 @@
 #Initialization 
 from flask import Flask 
+from flask import request 
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1> Hello, world! </h1>"
+    user_agent = request.headers.get('User-Agent')
+    return '<p> Your browser is {}</p'.format(user_agent)
 
 @app.route('/user/<name>')
 def user(name):
@@ -15,4 +18,4 @@ def user(name):
 
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
