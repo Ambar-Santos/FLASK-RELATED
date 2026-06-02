4db4a3bbcff87f1267229c5cc37cf110c41fe969
Ambar Santos <santosambar738@gmail.com>
2026-06-01 20:12:02 -0400

    adding forms with WTFforms

--- practice/app.py
+++ practice/app.py
@@ -1,18 +1,41 @@
 #Initialization 
 from flask import Flask, redirect, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'watapatapitusberry'
+
 bootstrap = Bootstrap(app)
 
+#Class for WTForms package 
+class NameForm(FlaskForm):
+   name = StringField('What is your name', validators=[DataRequired()])
+   submit = SubmitField('Submit')
+
+
+#Routes
+   
 @app.route('/')
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    return render_template('index.html', form=form)
 
 @app.route('/user/<user>')
-def user(user):
-    return render_template('user.html', user=user)
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+   return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+   return render_template('500.html'), 500
+ 
 
 
 if __name__ == '__main__':
