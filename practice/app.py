66a62013e02c9411c073acf33471bd3a7984086c
Ambar Santos <santosambar738@gmail.com>
2026-06-04 16:27:21 -0400

    so the query is not dynamically executed

--- practice/app.py
+++ practice/app.py
@@ -33,7 +33,7 @@ class Role(db.Model):
    __tablename__ = 'roles'
    id = db.Column(db.Integer, primary_key=True)
    name = db.Column(db.String(64), unique=True)
-   users = db.relationship('User', backref='role') #one to many 
+   users = db.relationship('User', backref='role', lazy='dynamic') #one to many 
 
    def __repr__(self):
       return '<Role%r>' % self.name 
