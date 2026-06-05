d3f738484ed7238fc53c4be6f2cb20cefd2ece1b
Ambar Santos <santosambar738@gmail.com>
2026-06-05 07:52:15 -0400

    CRUD operations
    1.Showring the entire list of items
    2. Creating a new record (item( with a form
    3. Updating an existing item
    4. Deleting an item

--- CRUD2/app.py
+++ CRUD2/app.py
@@ -10,3 +10,45 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.secret_key = 'a_very_secret_key'
 db = SQLAlchemy(app)
+
+#display all of the items
+#handling crud operations
+
+@app.route('/')
+def index():
+    items = Item.query.all()
+    return render_template('index.html', items=items)
+
+# Create new item 
+@app.route('/create', methods=['GET','POST'])
+def create():
+    form = ItemForm()
+    if form.valdate_on_submit():
+        item = Item(name=form.name.data, description=form.description.data)
+        db.session.add(item)
+        db.session.commit()
+        flash('Item created succesfully!')
+        return redirect(url_for('index'))
+    return render_template('create.html', form=form)
+      
+# Edit existing item (update)
+@app.rout('/edit/', method=['GET', 'POST'])
+def edit(id):
+    item = Item.query.get_or_404(id)
+    form = ItemForm(obj=item)
+    if form.validate_on_submit():
+        item.name = form.name.data
+        item.description = form.description.data
+        db.session.commit()
+        flash('Item succesfully updated!')
+        return redirect(url_for('index'))
+    return render_template('update.html', form=form, item=item)
+
+# Delete item 
+@app.route('/delete', methods=['POST'])
+def delete(id):
+    item = Item.query.get_or_404(id)
+    db.session.delete(item)
+    db.session.commit()
+    flash('Item deleted successfully!')
+    return redirect(url_for('index'))
