05e4a90e6ee8f30fe1519c0e832ae8e457b1a667
Ambar Santos <santosambar738@gmail.com>
2026-06-05 02:53:59 -0400

    Creating model and database

--- CRUD2/app.py
+++ CRUD2/app.py
@@ -2,10 +2,10 @@
 
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy 
-from forms import ItemForm 
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'sqlite:///items.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///items.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.secret_key = 'a_very_secret_key'
---
+++ CRUD2/models.py
@@ -0,0 +1,9 @@
+from app import db 
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(500), nullable=True)
+
+    def __repr__(self):
+        return f''
